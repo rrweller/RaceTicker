2bd243749918bde7dcadab99b8a252adebe1e877
rrweller <riley.r.weller@gmail.com>
2021-01-10 23:33:15 -0600

    Added app.png and threw in code from forum response
    Idk what I'm doing or if this is right

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 angular.module('beamng.apps')
-.directive('myApp', ['StreamsManager', function (StreamsManager) {
+.directive('RaceTicker', ['StreamsManager', function (StreamsManager) {
   return {
-    template:  '[Some HTML Content]',
+    template:  '<button ng-click="hello()">Click Me</button>',
     replace: true,
     restrict: 'EA',
     link: function (scope, element, attrs) {      
-      // An optional list of streams that will be used in the app
-      var streamsList = [/* streams here */];
- 
-      // Make the needed streams available.
-      StreamsManager.add(streamsList);
- 
-      // Make sure we clean up after closing the app.
-      scope.$on('$destroy', function () {
-        StreamsManager.remove(streamsList);
-      });
- 
-      scope.$on('streamsUpdate', function (event, streams) {
-        /* Some code that uses the streams' values */
-      });
+      scope.hello = function () {
+        // do something here.
+      };
     }
+	
+	//Creates a Lua global table in GameEngine Lua
+	bngApi.engineLua('script_state_table = {}');
+
+	//This is called all the time
+	scope.$on('streamsUpdate', function (event, streams) {
+        //This calls GameEngine Lua to tell all Vehicle Luas to insert their serialized ai.scriptState() into the GameEngine Lua script_state_table
+        bngApi.engineLua('be:queueAllObjectLua("obj:queueGameEngineLua(\'script_state_table[\'..obj:getID() .. \'] = \' .. serialize(ai.scriptState()))")');
+       
+        //This gets that script_state_table from GameEngine Lua
+        bngApi.engineLua('script_state_table', function(data) {
+            for (const [key, value] of Object.entries(data)) {
+                var veh_id = key;
+                var scriptTime = value.scriptTime;
+             
+                console.log("Vehicle ID: " + veh_id + ", Time: " + scriptTime);
+            }
+        });
+	});
   };
 }])
